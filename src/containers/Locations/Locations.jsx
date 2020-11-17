5b1c24852e7ad2dbef7285b6da9b2ba01f020653
Joseph Perry <josephperry720@gmail.com>
2020-11-17 12:53:41 -0500

    added switch case

--- src/containers/Locations/Locations.jsx
+++ src/containers/Locations/Locations.jsx
@@ -36,7 +36,24 @@ const Locations = () => {
   const maxWaterLevel = `${max} ft³/s`;
 
   const canvasRef = useRef(null);
-  const labelRef = useRef(null);
+
+  // changes fontawesome icon based on weather condition
+  const displayWeatherIcon = (weatherEvent) => {
+    switch (weatherEvent) {
+      case "Clouds":
+        return "fas fa-cloud";
+      case "Rain":
+        return "fas fa-cloud-rain";
+      case "Smoke" || "Haze" || "Fog" || "Mist":
+        return "fas fa-smog";
+      case "Thunderstorm":
+        return "fas fa-bolt";
+      case "Snow":
+        return "fas fa-snowflake";
+      default:
+        return "fas fa-sun";
+    }
+  };
 
   useEffect(() => {
     if (canvasRef.current) {
