c2e04b3b452c5a647aa87249c4c80ae1736616b7
Joseph Perry <josephperry720@gmail.com>
2020-12-11 15:56:11 -0500

    fixed current weather spacing

--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -4,11 +4,34 @@ import "./CurrentWeather.css";
 const CurrentWeather = ({ time, temp, description, iconClass }) => {
   return (
     <div id="current">
-      {/* <h2>Current Weather</h2> */}
-      <p>{time}</p>
-      <p>{temp}</p>
-      <p>{description}</p>
-      <i id="current-icon" className={`current-icon ${iconClass}`}></i>
+      <div className="row row-titles">
+        <div className="col-sm-4 text-center">
+          <h5>Time</h5>
+        </div>
+        <div className="col-sm-4 text-center">
+          <h5>Temperature</h5>
+        </div>
+        <div className="col-sm-4 text-center">
+          <h5>Weather</h5>
+        </div>
+      </div>
+      <hr/>
+      <div className="row">
+        <div className="col-sm-4 text-center">
+          <p>{time}</p>
+        </div>
+        <div className="col-sm-4 text-center">
+          <p>{temp}</p>
+        </div>
+        <div className="col-sm-4 text-center">
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="row icon-row">
+        <div className="col-sm-12 text-center">
+          <i id="current-icon" className={`current-icon ${iconClass}`}></i>
+        </div>
+      </div>
     </div>
   );
 };
